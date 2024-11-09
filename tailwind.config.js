ed2b010ca484b8dd6942cc9037a44c7d13ab897f
brandaoplaster <brandaoplaster@gmail.com>
2024-11-09 15:23:27 -0300

    feat: Adding theme and variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "var(--color-primary-light)",
+          DEFAULT: "var(--color-primary)",
+          dark: "var(--color-primary-dark)",
+        },
+        background: {
+          light: "var(--color-bg-light)",
+          DEFAULT: "var(--color-bg)",
+          dark: "var(--color-bg-dark)",
+        },
+        text: {
+          light: "var(--color-text-light)",
+          DEFAULT: "var(--color-text)",
+          dark: "var(--color-text-dark)",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
