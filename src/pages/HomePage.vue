747cfbebbcaad42ceae24a758a6904c83cd5266f
brandaoplaster <brandaoplaster@gmail.com>
2024-11-12 21:16:38 -0300

    feat: Adding data.json and list articles

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,31 +4,24 @@
     <ul>
       <li v-for="article in articles" :key="article.id" class="mb-4 p-4 border rounded-lg">
         <h2 class="text-xl font-semibold">{{ article.title }}</h2>
-        <p>{{ article.content }}</p>
+        <p>{{ article.title }}</p>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { Article } from '../types/article';
 
-const articles = ref([
-  { id: 1, title: "Artigo 1", content: "Este é o conteúdo do Artigo 1." },
-  { id: 2, title: "Artigo 2", content: "Este é o conteúdo do Artigo 2." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-  { id: 3, title: "Artigo 3", content: "Este é o conteúdo do Artigo 3." },
-]);
+const articles = ref<Article[]>([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch('data.json');
+    articles.value = await response.json();
+  } catch (error) {
+    console.error("Erro ao carregar o JSON:", error);
+  }
+});
 </script>
--- src/types/article.ts
+++ src/types/article.ts
@@ -1,7 +1,6 @@
 import { Category } from "./category";
 
 export interface Article {
-  id: number;
   title: string;
   path: string;
   publishedAt: string;
