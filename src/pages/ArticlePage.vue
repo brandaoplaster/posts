fbca13f9dced86ca4d98d0e792885ad7af21285c
brandaoplaster <brandaoplaster@gmail.com>
2024-11-13 22:02:32 -0300

    refactor: Refactoring article page to remove unnecessary code and using components

--- src/pages/ArticlePage.vue
+++ src/pages/ArticlePage.vue
@@ -1,26 +1,22 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { Article } from '../types/article';
 import { useRoute } from 'vue-router';
 import { marked } from 'marked';
-
+import LeftAside from "../components/LeftAside.vue";
 
 const article = ref<Article | null>(null);
 const articles = ref<Article[]>([]);
 const route = useRoute();
 const content = ref('');
+const computedCategories = computed(() => article.value?.categories ?? []);
 
 onMounted(async () => {
   const articleId = Number(route.params.id);
-  console.log(articleId)
 
   try {
     const response = await fetch("/posts/data.json");
-    console.log(response)
     articles.value = await response.json();
-
-    console.log("article take")
-    console.log(articles.value)
 
     article.value = articles.value.find((item: Article) => item.id === articleId) || null;
 
@@ -33,7 +29,6 @@ onMounted(async () => {
       content.value = await marked(markdownText);
     }
 
-    console.log("article load",article.value)
   } catch (error) {
     console.log("Error", error);
   }
@@ -49,39 +44,10 @@ onMounted(async () => {
         </h1>
       </header>
       <div class="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
+        <LeftAside :categories="computedCategories" />
         <div v-html="content" class="prose divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
 
         </div>
-        <footer>
-          <div class="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
-            <div class="py-4 xl:py-8">
-              <h2 class="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                Tags
-              </h2>
-              <div class="flex flex-wrap">
-                <div v-for="category in article?.categories" :key="category.name" class="mr-3">
-                  <a href="#"
-                    class="text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                    {{ category.name }}
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
-              <h2 class="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                Previus Article
-              </h2>
-              <div class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                New features in v1
-              </div>
-            </div>
-          </div>
-          <div class="pt-4 xl:pt-8">
-            <a href="#" class="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-              ← Back to the blog
-            </a>
-          </div>
-        </footer>
       </div>
     </article>
   </section>
