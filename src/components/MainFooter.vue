b6d539ba155bc11ede8f95eb2fdc4e3827a15be2
brandaoplaster <brandaoplaster@gmail.com>
2024-11-20 15:23:32 -0300

    refactor: Updating main footer with icons

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,14 +1,17 @@
 <script lang="ts">
-import { EnvelopeIcon } from "@heroicons/vue/20/solid";
+import MdiIcon from './MdiIcon.vue';
+import { mdiLinkedin, mdiGithub, mdiEmail } from '@mdi/js';
+
+
 
 export default {
   name: "MainFooter",
   components: {
-    EnvelopeIcon
+    MdiIcon
   },
   setup() {
     const currentYear = new Date().getFullYear();
-    return { currentYear };
+    return { currentYear, mdiLinkedin, mdiGithub, mdiEmail };
   },
 }
 </script>
@@ -17,7 +20,13 @@ export default {
     <footer class="mt-16 flex flex-col items-center">
     <div class="mb-3 flex space-x-4">
       <a href="#" class="text-sm text-gray-500 transition hover:text-gray-600">
-        <EnvelopeIcon class="w-6 h-6 text-yellow-500" />
+        <MdiIcon :icon="mdiLinkedin" />
+      </a>
+      <a href="#" class="text-sm text-gray-500 transition hover:text-gray-600">
+        <MdiIcon :icon="mdiGithub" />
+      </a>
+      <a href="#" class="text-sm text-gray-500 transition hover:text-gray-600">
+        <MdiIcon :icon="mdiEmail" />
       </a>
     </div>
     <div class="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
