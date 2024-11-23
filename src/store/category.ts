8e5181be371f4ef32ed0e1d7ac3946ef46f9b468
brandaoplaster <brandaoplaster@gmail.com>
2024-11-23 20:16:33 -0300

    feat: Add action laodCategories in store

--- src/store/category.ts
+++ src/store/category.ts
@@ -5,4 +5,19 @@ export const useCategoryStore = defineStore('category', {
   state: () => ({
     categories: [] as Category[],
   }),
+
+  actions: {
+    async loadCategories(): Promise<void> {
+      try {
+        const response = await fetch('/posts/data.json');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        this.categories = data.categories;
+      } catch (error) {
+        console.error('Error loading categories:', error);
+      }
+    },
+  }
 })
