62c1139d18af42843567c70019a544d8b8299e37
brandaoplaster <brandaoplaster@gmail.com>
2025-01-23 23:45:57 -0300

    fix: Fixing component positioning on screen

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import MainHeader from './components/MainHeader.vue';
 </script>
 
 <template>
-  <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+  <section class="flex flex-col min-h-screen mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
     <MainHeader />
-    <main class="container mx-auto pt-24">
+    <main class="flex-1 container mx-auto pt-24">
       <router-view />
     </main>
 
