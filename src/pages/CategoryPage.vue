08bdfafce42efa77ed21248efadbcad34ed2d01f
brandaoplaster <brandaoplaster@gmail.com>
2025-04-27 17:27:44 -0300

    feat: Add loding in category page

--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -10,15 +10,22 @@ const route = useRoute();
 const categoryName = computed(() => route.params.categoryName as string);
 
 const articles = ref<Article[]>([]);
+const isLoading = ref(false);
+const errorMessage = ref<string | null>(null);
 
 const loadArticles = async () => {
+  isLoading.value = true;
+  errorMessage.value = null;
   try {
     if (articleStore.articles.length === 0) {
       await articleStore.loadArticles("published");
     }
     articles.value = articleStore.getArticlesByCategory(categoryName.value);
   } catch (error) {
+    errorMessage.value = 'Erro ao carregar posts';
     console.log(error)
+  } finally {
+    isLoading.value = false
   }
 }
 
@@ -32,7 +39,6 @@ watch(
     loadArticles();
   }
 )
-
 </script>
 
 <template>
@@ -40,6 +46,13 @@ watch(
     <RightAside />
     <div class="divide-y divide-gray-200 dark:divide-gray-700 pt-6">
       <h1 class="text-2xl font-bold mb-4">Lista de Artigos</h1>
+      <div v-if="isLoading" class="loading">Carregando...</div>
+
+      <div v-else-if="error" class="error">{{ error }}</div>
+
+      <div v-else-if="!isLoading && articles.length === 0" class="no-posts">
+        Nenhum post encontrado para a categoria "{{ categoryName }}".
+      </div>
       <ul class="divide-y divide-gray-200 dark:divide-gray-700">
         <li v-for="article in articles" :key="article.title" class="py-12">
           <article>
@@ -56,9 +69,10 @@ watch(
                     </h2>
                     <div class="flex flex-wrap">
                       <div v-for="category in article.categories" :key="category.name" class="mr-3">
-                        <router-link :to="{ name: 'category', params: { categoryName: category.name } }" class="text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                        {{ category.name }}
-                    </router-link>
+                        <router-link :to="{ name: 'category', params: { categoryName: category.name } }"
+                          class="text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                          {{ category.name }}
+                        </router-link>
                       </div>
                     </div>
                   </div>
